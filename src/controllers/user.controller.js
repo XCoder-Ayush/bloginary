8b2ce932bda04c247c09ea8984a055745d0a8fc2
Ayush Sharma <arp23359@gmail.com>
2024-08-21 01:26:53 +0530

    fix: add user api for image upload and docker compose secrets

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,9 @@
 const UserService = require("../services/user.service");
-const cloudinary = require('../config/cloudinary.config');
-
+const cloudinary = require("../config/cloudinary.config");
 
 async function AddUser(request, response) {
   try {
-    await UserService.AddUser(request.body);
+    await UserService.AddUser(JSON.parse(request.body.userData), request.file);
     response.status(201).json({ message: "User Successfully Added." });
   } catch (error) {
     console.error("Error Saving User In Controller", error);
@@ -53,24 +52,24 @@ async function GetUserByUsername(request, response) {
   }
 }
 async function GetAllUsers(request, response) {
-    try {
-      // Your code to fetch user by ID
-      const users = await UserService.GetAllUsers();
-  
-      // If user not found
-      if (!users) {
-        console.error("No Users in DB : ");
-        response.status(404).json({ message: "Users Not Found" });
-        return;
-      }
-  
-      // If user found, return user data
-      response.status(200).json(users);
-    } catch (error) {
-      console.error("Internal Server Error:", error);
-      response.status(500).json({ message: "Internal Server Error" });
+  try {
+    // Your code to fetch user by ID
+    const users = await UserService.GetAllUsers();
+
+    // If user not found
+    if (!users) {
+      console.error("No Users in DB : ");
+      response.status(404).json({ message: "Users Not Found" });
+      return;
     }
+
+    // If user found, return user data
+    response.status(200).json(users);
+  } catch (error) {
+    console.error("Internal Server Error:", error);
+    response.status(500).json({ message: "Internal Server Error" });
   }
+}
 
 async function UpdatePassword(request, response) {
   try {
@@ -99,23 +98,23 @@ async function UpdatePassword(request, response) {
 const UpdateProfilePicture = async (req, res) => {
   try {
     // Check if file is uploaded
-    const id=req.params.id;
-    console.log(req.file);    
+    const id = req.params.id;
+    console.log(req.file);
 
     if (!req.file) {
       return res.status(400).json({ message: "No file uploaded" });
     }
-    console.log(req.file);    
-    
+    console.log(req.file);
+
     // Upload image to Cloudinary
     const result = await cloudinary.uploader.upload(req.file.path);
 
     // Delete the file from local server (optional)
     // fs.unlinkSync(req.file.path);
-    console.log(result)
-    
-    const user=await UserService.UpdateProfilePicture(id,result.secure_url)
-    
+    console.log(result);
+
+    const user = await UserService.UpdateProfilePicture(id, result.secure_url);
+
     return res.status(200).json(user);
   } catch (error) {
     console.error("Error Updating Profile Picture:", error);
@@ -123,4 +122,11 @@ const UpdateProfilePicture = async (req, res) => {
   }
 };
 
-module.exports = { AddUser, GetUserById, UpdatePassword, UpdateProfilePicture , GetAllUsers, GetUserByUsername};
+module.exports = {
+  AddUser,
+  GetUserById,
+  UpdatePassword,
+  UpdateProfilePicture,
+  GetAllUsers,
+  GetUserByUsername,
+};
--- src/routes/v1/routes/user.route.js
+++ src/routes/v1/routes/user.route.js
@@ -1,20 +1,24 @@
-const express=require('express')
+const express = require("express");
 
-const userRouter=express.Router();
-const UserController=require('../../../controllers/user.controller')
-const upload=require('../../../middlewares/multer.middleware')
-const { AuthMiddleware }=require('../../../middlewares/auth.middleware')
+const userRouter = express.Router();
+const UserController = require("../../../controllers/user.controller");
+const upload = require("../../../middlewares/multer.middleware");
+const { AuthMiddleware } = require("../../../middlewares/auth.middleware");
 
-userRouter.post('/',UserController.AddUser)
+userRouter.post("/", upload.single("file"), UserController.AddUser);
 // userRouter.post('/',()=>{
 //     console.log(request.body);
 //     response.json({'message' : 'User Successfully Added.'}).status(201)
 // })
 
 // userRouter.get('/:id',UserController.GetUserById)
-userRouter.get('/:username',UserController.GetUserByUsername)
-userRouter.get('/', AuthMiddleware, UserController.GetAllUsers)
-userRouter.patch('/',UserController.UpdatePassword)
-userRouter.put('/upload/:id',upload.single('file'),UserController.UpdateProfilePicture)
+userRouter.get("/:username", UserController.GetUserByUsername);
+userRouter.get("/", AuthMiddleware, UserController.GetAllUsers);
+userRouter.patch("/", UserController.UpdatePassword);
+userRouter.put(
+  "/upload/:id",
+  upload.single("file"),
+  UserController.UpdateProfilePicture
+);
 
-module.exports=userRouter;
+module.exports = userRouter;
